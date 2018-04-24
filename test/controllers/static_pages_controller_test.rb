ed49ad4252eed9b9189755348a6c2d76a11cf9c5
Ben <bchadwick94@gmail.com>
2018-04-24 20:57:48 +0100

    rails exercises

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@basetitle}"
+    assert_select "title", "#{@basetitle}"
   end
 
   test "should get help" do
